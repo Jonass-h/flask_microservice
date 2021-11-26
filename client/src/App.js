402a4938c6a783e769cd31c5f886ea912d37d19a
younes-h <younesyuuyuu@gmail.com>
2021-11-27 00:49:22 +0100

    client changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import axios from "axios";
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <p>My Token = {window.token}</p>
-    </div>
-  );
+
+const api=axios.create({
+  baseURL:"http://127.0.0.1:5100"
 }
+)
+
+export default class App extends Component {
+  state={
+    projects=[
+    ]
+  }
+
+  constructor(){
+    super();
+    api.get("/").then(
+      res => {
+        console.log(res)
+        this.setState({})
+      }
+    )
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
 
 export default App;
